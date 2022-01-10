467015796cc43bc485e22c2dfe74c29402e611a5
MurtazaBagwala07 <murtazabag786@gmail.com>
2022-01-11 02:38:41 +0530

    easy part done

--- BuildRepo/Operators, Branching, Loops/Easy/Easy.js
+++ BuildRepo/Operators, Branching, Loops/Easy/Easy.js
@@ -1,1 +1,53 @@
-console.log("hello world")
+// 1.Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
+// const sum=(...args)=>{
+//     let sum=0;
+//     args.forEach(arg=>{
+//         sum+=arg;
+//     })
+//     return sum;
+// }
+// console.log(sum(5,13,7,21,48))
+
+// 2.Write a program to take a number input from user and determine whether the number is odd or even.
+// const oddOrEven =(num)=>num%2==0?'even':"odd";
+// console.log(oddOrEven(7))
+
+//3.Write a program to find the maximum and minimum out of two given numbers. The numbers are num1=129 and num2=251.
+// const bigNumber=(a,b)=>a>b?a:b;
+// console.log(bigNumber(10,7))
+
+// 4.Write a program to find the maximum out of three given numbers. The numbers are num1=8, num2=23 and num3=17.
+// const biggerNumber=(a,b,c)=>{
+//     if(a>b&&a>c)
+//         return a;
+//     else if(b>c&&b>a)
+//         return b;
+//     else
+//         return c;
+// }
+// console.log(biggerNumber(2,3,9))
+
+
+// 5.Write a program to find the minimum out of three given numbers. The numbers are num1=35, num2=29 and num3=46.
+// const smallerNumber =(a,b,c)=>{
+//     if(a<b&&a<c)
+//         return a;
+//     else if(b<c&&b<a)
+//         return b;
+//     else
+//         return c;
+// }
+// console.log(smallerNumber(2,3,9))
+
+
+// 6.Write program to take a month as an input from the user and find out whether the month has 31 days or not.
+// const month31OrNot=(month)=>{
+//     if(month==1||month==3||month==5||month==7||month==8||month==10||month==12){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(month31OrNot(5))

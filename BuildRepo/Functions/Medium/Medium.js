9cbf5d7403d7de076f7d6fece92c2d98770f4309
MurtazaBagwala07 <murtazabag786@gmail.com>
2022-01-11 12:19:38 +0530

    not complete yet

---
+++ BuildRepo/Functions/Medium/Medium.js
@@ -0,0 +1,38 @@
+// 1.Given an array, your function should return the length of the array.
+// Example:
+// Input: arrayLength([1,5,3,7,8]) ––> Output: 5
+// const arrayLength =(array)=>{
+//     let length=0;
+//     array.map(num=>{
+//         length++;
+//     })
+//     return length;
+// }
+// console.log(arrayLength([3,4,5,5,6,43,2,45,6]));
+
+
+// #Doubt
+// 2.Given an array and an item, your function should return the index at which the item is present.
+// Example:
+// Input: indexOf([1,6,3,5,8,9], 3) ––> Output: 2
+// const indexOf = (array,item)=>{
+//     array.map((num,index)=>{
+//         if(num===item){
+//             return index;
+//         }
+//     })
+//     return  "not found"
+// }
+// console.log(indexOf([1,2,3,4,5,6],4))
+
+
+// 3.Given an array and two numbers, your function should replace all entries of first number in an array with the second number.
+// Example:
+// Input: replace([1,5,3,5,6,8], 5, 10) ––> Output: [1,10,3,10,6,8]
+// const replace=(array,num,repl)=>{
+//     array.map(arg=>{
+        
+//     })
+// }
+
+
